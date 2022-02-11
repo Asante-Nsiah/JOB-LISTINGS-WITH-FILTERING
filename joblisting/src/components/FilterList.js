f7823cc5f7a5df710419977da01197d09caae3f5
Alhassan <alhassan.razak@amalitech.org>
2022-02-11 06:36:27 +0000

    responsive_update

--- joblisting/src/components/FilterList.js
+++ joblisting/src/components/FilterList.js
@@ -1,17 +1,12 @@
-import React from "react";
 
-const Filters = ({ activeFilters, handleRemove, clearFilters }) => {
-  const classes = {
-    wrapper:
-      "bg-white w-10/12 max-w-6xl p-8 flex items-center justify-between w-100 transform translate-y-1/2 rounded",
-    list: "flex gap-4 flex-wrap",
-    filterItem:
-      "bg-cyan-light text-cyan-default px-2 pt-2 pb-1 pr-10 rounded text-sm font-bold relative",
-    removeButton:
-      "text-white bg-cyan-default w-8 h-8 absolute top-0 right-0 rounded-r",
-    clearButton: "text-cyan-default font-bold text-s",
-  };
-};
+    // wrapper:
+    //   "bg-white w-10/12 max-w-6xl p-8 flex items-center justify-between w-100 transform translate-y-1/2 rounded",
+    // list: "flex gap-4 flex-wrap",
+    // filterItem:
+    //   "bg-cyan-light text-cyan-default px-2 pt-2 pb-1 pr-10 rounded text-sm font-bold relative",
+    text-white bg-cyan-default w-8 h-8 absolute top-0 right-0 rounded-r,
+    text-cyan-default font-bold text-s,
+
 
 return (
   <div className={classes.wrapper}>
